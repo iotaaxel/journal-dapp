157fd81ae2b59bd5899742cf5becfe8bf8a2dbe5
Axel Iota <iota.axel@gmail.com>
2022-02-10 00:37:34 -0500

    Unwrap struct for access

--- contracts/Journal.sol
+++ contracts/Journal.sol
@@ -4,9 +4,6 @@ pragma solidity 0.8.11;
 
 contract Journal {
 
-    address public owner;
-    string public title;
-
     enum Mood { Anxious, Happy, Sad, Melancholy}
 
     struct Entry {
@@ -14,26 +11,38 @@ contract Journal {
         string note; // write how you are feeling, etc.
         address owner;
         Mood mood;
+        uint entry_id;
     }
+
+    Entry entry;
 
     uint public entryCount = 0;
     mapping(uint => Entry) public entries;
 
-    event Submitted(string title, string note, address owner, Mood mood);
+    event Submitted(Entry);
 
     function createJournalEntry(string memory _note, string memory _title, Mood _mood) public {
         // Require valid entry items
         require(bytes(_note).length > 0 && bytes(_title).length > 0);
     
         // Create a journal entry
-        entries[entryCount] = Entry(_title, _note, msg.sender, _mood);
+        entry = Entry(_title, _note, msg.sender, _mood, entryCount);
         // Trigger event
-        emit Submitted(_title, _note, msg.sender, _mood);
+        emit Submitted(entry);
         // Increase entry count
         entryCount++;
     }
 
     function getTitle() public view returns (string memory) {
-        return (title);
+        return entry.title;
     }
+
+    function getNote() public view returns (string memory) {
+        return entry.note;
+    }
+
+    function getMood() public view returns (Mood) {
+        return entry.mood;
+    }
+
 }
